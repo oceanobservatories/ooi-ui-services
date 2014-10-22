43f4f3711effdc74618014c8b5034647dff17fcd
Matt <mjc@Matthews-MacBook-Air-4.local>
2014-10-22 11:57:30 -0400

    PlatformModel created

---
+++ ooiservices/model/platform.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+'''
+ooiservices.model.platform.py
+
+Platform resource
+
+'''
+
+from model.base import BaseModel
+
+
+__author__ = "Matt Campbell"
+
+class PlatformModel(BaseModel):
+
+
+	__cols__ = ['id']
+
+	id = None
+
+	def __init__(self):
+		BaseModel.__init__(self)
+
+
+	def _check(self):
+		'''
+		Will be used to check the parcel that the contents is worth using.
+		'''
+		pass
+
+	'''
+	External CRUD implementation from BaseModel
+	'''
+    def create(self, obj):
+        createResult = adapter.create(obj)
+        return createResult
+
+    def read(self, id):
+        readResult = adapter.read(id)
+		return readResult
+
+    def update(self, obj):
+		updateResult = adapter.update(obj)
+		return updateResult
+
+    def delete(self, id):
+		deleteResult = adapter.delete(id)
+		return deleteResult
