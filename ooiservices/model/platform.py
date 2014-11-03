d0e009fcdb297ebb69110be365735b6624f758c0
Matt <mjc@air.local>
2014-11-03 10:48:43 -0500

    using db sample andy provided.

--- ooiservices/model/platform.py
+++ ooiservices/model/platform.py
@@ -11,11 +11,4 @@ __author__ = "Matt Campbell"
 class PlatformModel(SqlModel):
 
     def __init__(self):
-        SqlModel.__init__(self, 'platforms')
-
-
-    def read(self):
-        #TODO: this isn't working quite right, it's not gouping by platform id
-        query = 'SELECT arrays.name, nodes.name, sites.name, subsites.name FROM arrays, nodes, sites, subsites, platforms WHERE arrays.id=platforms.array AND nodes.id=platforms.node AND sites.id=platforms.node AND subsites.id=platforms.subsite ORDER BY platforms.id;'
-        answer = self.sql.perform(query)
-        return answer
+        SqlModel.__init__(self, 'ooi_platforms')
