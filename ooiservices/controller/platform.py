a8eed6880c73756f32e17610d9afd3abc4bdcfc7
Matthew Campbell <mjc@Matthews-iMac.local>
2014-11-05 12:24:13 -0500

    Put back methods for platform controller

--- ooiservices/controller/platform.py
+++ ooiservices/controller/platform.py
@@ -11,25 +11,41 @@ from ooiservices.model.platform import PlatformModel
 from flask.ext.restful import Resource
 from flask import request
 
-#TODO: BaseController already does this
-#from ooiservices.model.base import BaseModel
-
 __author__ = "Brian McKenna"
 
 class PlatformController(BaseController):
-    
-    #    def _model(adapter):
-    #        return BaseModel() # TODO
     
     plat = PlatformModel()
     
     def __init__(self):
         BaseController.__init__(self)
 
-    class List(Resource):
+    def get(self, id):
+        result = self.plat.read(id)
+        #Formats the json dict to be used by the view:
+        formatted_result = self.plat.filter_ooi_platforms(result)
+        return formatted_result
+
+    def put(self, id):
+        args = request.args
+        params = args.items()
+        doc = {}
+        if params:
+            for item in params:
+                doc[item[0]] = item[1]
+        doc['id'] = id
+            result = self.plat.update(doc)
+        return result
+
+    def delete(self, id):
+        return self.plat.delete(id)
+   
+   class List(Resource):
         plat = PlatformModel()
         
         def get(self):
             result = self.plat.read()
+            #Formats the json dict to be used by the view:
             formatted_result = self.plat.filter_ooi_platforms(result)
+
             return formatted_result
