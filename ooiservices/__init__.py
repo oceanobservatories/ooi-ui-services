f8714004507f7ef8661933fe86d5d943d78ea35f
Dan Maher <dmaher@asascience.com>
2014-11-11 15:24:17 -0500

    Adding logging capabilities to __init__.py . Added log file

--- ooiservices/__init__.py
+++ ooiservices/__init__.py
@@ -6,8 +6,26 @@ Initializes the application and necessary application logic
 '''
 from flask import Flask
 
-
 app = Flask(__name__)
 
 import ooiservices.routes # initialize the routes
+
+
+
+#This line should be moved to a defaults.py in the future
+app.config['LOG_FILE'] = True
+#Adding logging capabilities.
+if app.config.get('LOG_FILE') == True:
+    import logging
+    logger = logging.getLogger('replicate')
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler('logs/ooiservices.txt')
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(process)d - %(name)s - %(module)s:%(lineno)d - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler) 
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.info('Application Process Started')
 
