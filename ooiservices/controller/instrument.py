b72f43c5f54ce56b2ff4dc46efa481dcface4675
Matthew Campbell <mjc@Matthews-iMac-5.local>
2014-11-13 16:00:25 -0500

    need request

---
+++ ooiservices/controller/instrument.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+'''
+ooiservices.controller.instrument
+
+InstrumentController
+'''
+
+from ooiservices.controller.base import BaseController
+from ooiservices.model.instrument import InstrumentModel
+
+__author__ = "Matt Campbell"
+
+class InstrumentController(BaseController):
+
+    inst = InstrumentModel()
+
+    def __init__(self):
+        BaseController.__init__(self)
+
+    def get(self):
+        args = request.args
+        return 'GET OK (list)'
