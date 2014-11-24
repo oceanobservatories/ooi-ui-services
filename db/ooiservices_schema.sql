3b8283248313f8f55eacca825e0f3c3c34196c31
Matt <mjc@mjc-air-3.local>
2014-11-24 14:35:54 -0500

    generalized the type cast of the geography column to just ‘geography’

--- db/ooiservices_schema.sql
+++ db/ooiservices_schema.sql
@@ -5,7 +5,7 @@
 CREATE TABLE arrays (
     id          VARCHAR(30) NOT NULL,
     description VARCHAR(255),
-    geography   geography(POINT,4326),
+    geography   geography,
 
     PRIMARY KEY (id)
 );
@@ -14,7 +14,7 @@ CREATE TABLE platforms (
     id          VARCHAR(30) NOT NULL,
     array_id    VARCHAR(30) NOT NULL,
     description VARCHAR(255),
-    geography   geography(POINT,4326),
+    geography   geography,
 
     PRIMARY KEY (id),
     FOREIGN KEY (array_id) REFERENCES arrays(id)
@@ -24,7 +24,7 @@ CREATE TABLE instruments (
     id          VARCHAR(30) NOT NULL,
     platform_id VARCHAR(30) NOT NULL,
     description VARCHAR(255),
-    geography   geography(POINT,4326),
+    geography   geography,
 
     PRIMARY KEY (id),
     FOREIGN KEY (platform_id) REFERENCES platforms(id)
