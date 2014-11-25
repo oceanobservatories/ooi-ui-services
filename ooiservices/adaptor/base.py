a02dc52e834f0f1483e1f70dd107d353721e6183
Edna Donoughe <ednapa01@gmail.com>
2014-11-25 15:04:53 -0500

    update erddap model to use CacheModel and RestAdaptor

--- ooiservices/adaptor/base.py
+++ ooiservices/adaptor/base.py
@@ -4,10 +4,18 @@ ooiservices.adaptor.base
 
 Definitions for the BaseAdaptor 
 '''
+from flask import make_response
+from flask import jsonify
 
 class BaseAdaptor:
     '''
     Base class for adaptor implementations
     '''
-    pass
+    def response_HTTP204(self):
+        return make_response('', 204)
 
+    def response_HTTP404(self):
+        return make_response(jsonify({'error': 'Not Found'} ), 404)
+
+    def response_HTTP500(self):
+        return make_response(jsonify({'error': 'internal server error'} ), 500)
---
+++ ooiservices/controller/erddap.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+'''
+ooiservices.controller.erddap.py
+
+ErddapController
+
+'''
+
+from ooiservices.controller.base import ObjectController
+from ooiservices.controller.base import ListController
+from ooiservices.model.erddap import ErddapModel
+
+__author__ = "Edna Donoughe"
+
+class ErddapObjectController(ObjectController):
+
+    def __init__(self):
+        ObjectController.__init__(self)
+        try:
+            self.model = ErddapModel()
+        except Exception as e:
+            raise Exception('erddap controller __init__: %s' % e)
+
+    def get(self, id):
+        try:
+            result = self.model.read(id)
+            if not result:
+                result = self.response_HTTP204()
+        except:
+            result = self.response_HTTP500()
+        return result
+
+    def put(self, obj):
+        raise NotImplementedError('erddap ObjectController put - Not Implemented')
+
+    def delete(self, id):
+        result = None
+        try:
+            if id:
+                result = self.model.delete(id)
+                if not result:
+                    result = self.response_HTTP204()
+        except Exception as e:
+            #print '%s' % e
+            result = self.response_HTTP500()
+        return result
+
+class ErddapListController(ListController):
+
+    def __init__(self):
+        ListController.__init__(self)
+        self.model = ErddapModel()
+
+    def get(self):
+        raise NotImplementedError('erddap ListController get - Not Implemented')
