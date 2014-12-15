3c858191b89a6ff22050015091ec889113364fb6
Matt <mjc@mjc-air-2.local>
2014-12-14 22:50:22 -0500

    Ednas updates to the pooling

--- ooiservices/model/adaptor/postgres.py
+++ ooiservices/model/adaptor/postgres.py
@@ -18,34 +18,47 @@ class PostgresAdaptor(object):
     password = None
     host     = None
     port     = None
+    minconn  = None
+    maxconn  = None
     def __init__(self, database=None, username=None, password=None, host=None, port=None):
         object.__init__(self)
         self.database = database
         self.username = username
         self.password = password
-        self.host = host
-        self.port = port
-        self.pool = self._getpool()
-    #Helper function to ensure the database connection.  Formerly getDB()
+        self.host     = host
+        self.port     = port
+        self.minconn  = 1
+        self.maxconn  = 10
+        self.pool     = self._getpool()
     def _getpool(self):
+        '''
+        create simple connection pool
+        '''
         try:
-            pool = psycopg2.pool.SimpleConnectionPool(1,10,database=self.database, user=self.username, password=self.password, host=self.host, port=self.port)
+            pool = psycopg2.pool.SimpleConnectionPool(self.minconn,self.maxconn,database=self.database, user=self.username, password=self.password, host=self.host, port=self.port)
         except psycopg2.DatabaseError, e:
             raise Exception('<PostgresAdaptor> getpool failed (check config settings): %s: ' % e)
         except:
             raise Exception('<PostgresAdaptor> getpool (check config settings)')
         return pool
-
     @contextmanager
     def _getcursor(self):
-        con = self.pool.getconn()
+        '''
+        get a connection from connection pool, yield cursor, perform commit if successful
+        (rollback if exception); finally return connection to the pool.
+        '''
+        conn = self.pool.getconn()
         try:
-            yield con.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+            yield conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+            conn.commit()
+        except:
+            conn.rollback()
         finally:
-            self.pool.putconn(con)
-
+            self.pool.putconn(conn)
     def perform(self, query, *args):
-        #Create a cursor_factory to return dictionary
+        '''
+        Get cursor factory (to return dictionary), execute query and return result
+        '''
         try:
             with self._getcursor() as cursor:
                 for item in args:
@@ -53,5 +66,4 @@ class PostgresAdaptor(object):
                 result = cursor.fetchall()
         except psycopg2.DatabaseError, e:
             raise Exception('<PostgresAdaptor> perform failed: %s: ' % e)
-
         return result
