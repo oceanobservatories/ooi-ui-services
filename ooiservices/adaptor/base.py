867755cf71405d415be3380b10ba929ab7d2a604
Matt <mjc@mjc-air-2.local>
2014-12-15 16:21:22 -0500

    merge pull request

--- ooiservices/adaptor/base.py
+++ ooiservices/adaptor/base.py
@@ -2,7 +2,7 @@
 '''
 ooiservices.adaptor.base
 
-Definitions for the BaseAdaptor 
+Definitions for the BaseAdaptor
 '''
 from flask import make_response
 from flask import jsonify
@@ -11,11 +11,4 @@ class BaseAdaptor:
     '''
     Base class for adaptor implementations
     '''
-    def response_HTTP204(self):
-        return make_response('', 204)
-
-    def response_HTTP404(self):
-        return make_response(jsonify({'error': 'Not Found'} ), 404)
-
-    def response_HTTP500(self):
-        return make_response(jsonify({'error': 'internal server error'} ), 500)
+    pass
