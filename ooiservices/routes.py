7fb32fb0552b2dc504516555512443c0ed10d730
Matt <mjc@mjc-air.local>
2014-12-24 10:36:07 -0500

    implemented the user model/controller, and updated routes to expose an internal (flask) and external (jsonp) endpoint.  No schema update at this time.

--- ooiservices/routes.py
+++ ooiservices/routes.py
@@ -10,9 +10,12 @@ from ooiservices import app
 from config import extend_json
 
 
-api = restful.Api(app)
-extend_json.support_jsonp(api)
+jsonp_api = restful.Api(app)
+flask_api = restful.Api(app)
 
+extend_json.support_jsonp(jsonp_api)
+
+#jsonp classes
 from ooiservices.controller.platform import PlatformObjectController, PlatformListController
 from ooiservices.controller.instrument import InstrumentObjectController, InstrumentListController
 from ooiservices.controller.array import ArrayObjectController, ArrayListController
@@ -22,25 +25,33 @@ from ooiservices.controller.stream import StreamObjectController, StreamListCont
 from ooiservices.controller.instrument_deployment import InstrumentDeploymentListController, InstrumentDeploymentObjectController
 from ooiservices.controller.platform_deployment import PlatformDeploymentListController, PlatformDeploymentObjectController
 
-# endpoints
-api.add_resource(ArrayListController, '/arrays')
-api.add_resource(ArrayObjectController, '/arrays/<string:id>')
+#flask classes
+from ooiservices.controller.user import UserAdd, UserLogin
 
-api.add_resource(PlatformListController, '/platforms')
-api.add_resource(PlatformObjectController, '/platforms/<string:id>')
+# endpoints for jsonp (client side) requests.  Unsecure.  GET methods only.
+jsonp_api.add_resource(ArrayListController, '/arrays')
+jsonp_api.add_resource(ArrayObjectController, '/arrays/<string:id>')
 
-api.add_resource(InstrumentListController, '/instruments')
-api.add_resource(InstrumentObjectController, '/instruments/<string:id>')
+jsonp_api.add_resource(PlatformListController, '/platforms')
+jsonp_api.add_resource(PlatformObjectController, '/platforms/<string:id>')
 
-api.add_resource(InstrumentDeploymentListController, '/instrument_deployments')
-api.add_resource(InstrumentDeploymentObjectController, '/instrument_deployments/<string:id>')
+jsonp_api.add_resource(InstrumentListController, '/instruments')
+jsonp_api.add_resource(InstrumentObjectController, '/instruments/<string:id>')
 
-api.add_resource(PlatformDeploymentListController, '/platform_deployments')
-api.add_resource(PlatformDeploymentObjectController, '/platform_deployments/<string:id>')
+jsonp_api.add_resource(InstrumentDeploymentListController, '/instrument_deployments')
+jsonp_api.add_resource(InstrumentDeploymentObjectController, '/instrument_deployments/<string:id>')
 
-api.add_resource(StreamListController, '/streams')
-api.add_resource(StreamObjectController, '/streams/<string:id>')
+jsonp_api.add_resource(PlatformDeploymentListController, '/platform_deployments')
+jsonp_api.add_resource(PlatformDeploymentObjectController, '/platform_deployments/<string:id>')
 
-api.add_resource(ParameterListController, '/parameters')
+jsonp_api.add_resource(StreamListController, '/streams')
+jsonp_api.add_resource(StreamObjectController, '/streams/<string:id>')
 
-api.add_resource(ErddapObjectController, '/erddap/<string:id>')
+jsonp_api.add_resource(ParameterListController, '/parameters')
+
+jsonp_api.add_resource(ErddapObjectController, '/erddap/<string:id>')
+
+# endpoints for Flask (server side) requests.  Secure. All supported methods.
+#TODO, may need Brian M. to look at the logic of these routes.
+flask_api.add_resource(UserAdd, '/user_add/<string:id>')
+flask_api.add_resource(UserLogin, '/user_login/<string:id>')
