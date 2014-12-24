c493f875f0d3505440ca10b59e3f0cabf9e9ff7c
Matt <mjc@mjc-air.local>
2014-12-24 10:36:07 -0500

    implemented the user model/controller, and updated routes to expose an internal (flask) and external (jsonp) endpoint.  No schema update at this time.

---
+++ ooiservices/controller/user.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+'''
+ooiservices.controller.user
+
+UserAdd
+UserLogin
+'''
+
+from ooiservices.controller.base import ObjectController
+from ooiservices.model.user import UserModel
+from flask import request
+from flask.ext.restful import Resource
+
+__author__ = "Matt Campbell"
+
+class UserAdd(ObjectController):
+
+    model = UserModel()
+
+    def __init__(self):
+        ObjectController.__init__(self)
+
+    def put(self, id):
+        result = self.model.read(id)
+        if not result:
+            return self.response_HTTP204()
+        return result
+
+    def get(self, id):
+        result = self.model.read(id)
+        if not result:
+            return self.response_HTTP204()
+        return result
+
+class UserLogin(ObjectController):
+
+    model = UserModel()
+
+    def __init__(self):
+        ObjectController.__init__(self)
+
+    def get(self, id):
+        result = self.model.read(id)
+        if not result:
+            return self.response_HTTP204()
+        return result
+
+    def get(self, id):
+        result = self.model.read(id)
+        if not result:
+            return self.response_HTTP204()
+        return result
