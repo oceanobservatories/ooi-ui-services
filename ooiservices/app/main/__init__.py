56432fb20f3f066690f4cedb9d8cacdf5a6eb0f7
Matt <mjc@mjc-air.local>
2015-01-11 21:33:27 -0500

    updating tests to use the right reqiurements and db

--- ooiservices/app/main/__init__.py
+++ ooiservices/app/main/__init__.py
@@ -9,10 +9,3 @@ __author__ = 'M.Campbell'
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
-
-#from ..models import Permission
-
-
-@main.app_context_processor
-def inject_permissions():
-    return dict(Permission=Permission)
