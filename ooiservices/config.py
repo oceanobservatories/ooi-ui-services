3adc99939ff9ae40bccb3b07213ec11776480793
James Case <case@oceanz.org>
2015-01-17 10:38:36 -0500

    AWS RDS connection string neutralization.

--- ooiservices/config.py
+++ ooiservices/config.py
@@ -32,7 +32,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-    'postgres://asa:asa2015@ooiui-dev.cvyc5bvl5zzs.us-east-1.rds.amazonaws.com/ooiuidev'
+    'postgres://user:password@hostname/database_name'
 
 class LocalDevelopmentConfig(Config):
     DEBUG = True
