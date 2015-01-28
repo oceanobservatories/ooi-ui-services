5e55a4d8ee0ae5637e35b3e1c7d7ab712bfa8607
Matt <mjc@mjc-air.local>
2015-01-28 13:41:19 -0500

    added PUT method to updated annotations, supply annotation id.  If the current user is the authoer then they have default permission to edit.  Otherwise they'll need a scope.  new scope added as a filler: 'annotate'

--- ooiservices/app/main/annotation.py
+++ ooiservices/app/main/annotation.py
@@ -5,7 +5,7 @@ Annotation endpoints
 '''
 __author__ = 'M@Campbell'
 
-from flask import jsonify, request, current_app, url_for
+from flask import jsonify, request, current_app, url_for, g
 from ooiservices.app.main import api
 from ooiservices.app import db
 from authentication import auth
@@ -21,7 +21,7 @@ def get_annotations():
 @auth.login_required
 @scope_required('annotate')
 def set_annotation():
-    annotation = Annotation.from_json(request.json)
+    annotation = Annotation.new_from_json(request.json)
     db.session.add(annotation)
     db.session.commit()
     return jsonify(annotation.to_json())
@@ -29,4 +29,15 @@ def set_annotation():
 @api.route('/annotations/<string:id>')
 def get_annotation(id):
     annotation = Annotation.query.filter_by(user_name=id).first_or_404()
+    return jsonify(annotation.to_json())
+
+@api.route('/annotations/<int:id>', methods=['PUT'])
+@scope_required('annotate')
+def edit_annotation(id):
+    annotation = Annotation.query.get_or_404(id)
+    if g.current_user != annotation.user_name and \
+            not g.current_user.can('annotate'):
+        return forbidden('Insufficient permissions')
+    annotation.comment = request.json.get('comment', annotation.comment)
+    db.session.add(annotation)
     return jsonify(annotation.to_json())
