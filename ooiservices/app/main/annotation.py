dc092209f26ead74b7ae60c4d15ffe8a6957a0bd
Matt <mjc@mjc-air.local>
2015-01-28 09:57:50 -0500

    placeholder for annotation routes.  Moved asset management routes to asset.py

---
+++ ooiservices/app/main/annotation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+Annotation endpoints.
+
+'''
+__author__ = 'M@Campbell'
+
+from flask import jsonify, request, current_app, url_for
+from ooiservices.app.main import api
+from ooiservices.app import db
+from authentication import auth
+from ooiservices.app.models import Annotation
+
+#Placeholder
