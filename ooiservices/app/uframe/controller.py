ee5a138b2fc29a0dd2c8d00c5ba480592ae14a27
Matt <mjc@mjc-air.local>
2015-02-01 14:24:56 -0500

    refactor uframe controller: move back non uframe endpoints to routes.py, updated README for POST annotations.

--- ooiservices/app/uframe/controller.py
+++ ooiservices/app/uframe/controller.py
@@ -96,16 +96,6 @@ def get_data(stream, instrument):
                 c_r.append({"f":str_date,"v":date_str})
                 if field == pref_timestamp:
                     time_idx = len(c_r)-1
-            elif field == ("annotations"):
-                if d['dofst_k_oxygen']>3277:
-                    c_r.append({"v":"test"})
-                else:
-                    c_r.append({"v":None})
-            elif field == ("annotationText"):
-                if d['dofst_k_oxygen']>3277:
-                    c_r.append({"v":"test"})
-                else:
-                    c_r.append({"v":None})
             else:
                 c_r.append({"v":d[field],"f":d[field]})
         some_data.insert(0,{"c":c_r})
@@ -120,32 +110,3 @@ def get_data(stream, instrument):
                  }
 
     return jsonify(**resp_data)
-
-@api.route('/platformlocation', methods=['GET'])
-def get_platform_deployment_geojson_single():
-    geo_locations = {}
-    if len(request.args) > 0:
-        if ('reference_designator' in request.args):
-            if len(request.args['reference_designator']) < 100:
-                reference_designator = request.args['reference_designator']
-                geo_locations = PlatformDeployment.query.filter(PlatformDeployment.reference_designator == reference_designator).all()
-    else:
-        geo_locations = PlatformDeployment.query.all()
-    if len(geo_locations) == 0:
-        return '{}', 204
-    return jsonify({ 'geo_locations' : [{'id' : geo_location.id, 'reference_designator' : geo_location.reference_designator, 'geojson' : geo_location.geojson} for geo_location in geo_locations] })
-
-@api.route('/display_name', methods=['GET'])
-def get_display_name():
-    # 'CE01ISSM-SBD17'
-    platform_deployment_filtered = None
-    display_name = ''
-    if len(request.args) > 0:
-        if ('reference_designator' in request.args):
-            if len(request.args['reference_designator']) < 100:
-                reference_designator = request.args['reference_designator']
-                platform_deployment_filtered = PlatformDeployment.query.filter_by(reference_designator=reference_designator).first_or_404()
-                display_name = platform_deployment_filtered.proper_display_name
-    if platform_deployment_filtered is None:
-        return '{}', 204
-    return jsonify({ 'proper_display_name' : display_name })
