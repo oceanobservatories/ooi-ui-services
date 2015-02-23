1b0571dd0b2d2642210b93a3384ebb1c5bf53c54
Matt <mjc@mjc-air.local>
2015-02-22 23:17:03 -0500

    tired of using jsonify, using decorator.  It was a good exercise

--- ooiservices/app/main/search.py
+++ ooiservices/app/main/search.py
@@ -9,21 +9,26 @@ from ooiservices.app.main import api
 from ooiservices.app import db
 from ooiservices.app.main.authentication import auth
 from ooiservices.app.models import PlatformDeployment, Organization, User
+from ooiservices.app.decorators import json
+
 
 @auth.login_required
 @api.route('/search/platform_deployments/display_name/<string:id>')
+@json
 def search_pds_display_name(id):
     platform_deployments = PlatformDeployment.query.whoosh_search(id)
-    return jsonify({ 'search_results' : [platform_deployment.to_json() for platform_deployment in platform_deployments] })
+    return { 'search_results' : [platform_deployment.to_json() for platform_deployment in platform_deployments] }
 
 @auth.login_required
 @api.route('/search/organizations/organization_name/<string:id>')
+@json
 def search_orgs_display_name(id):
     organizations = Organization.query.whoosh_search(id)
-    return jsonify({ 'search_results' : [organization.to_json() for organization in organizations] })
+    return { 'search_results' : [organization.to_json() for organization in organizations] }
 
 @auth.login_required
 @api.route('/search/users/<string:id>')
+@json
 def search_users_display_name(id):
     users = User.query.whoosh_search(id)
-    return jsonify({ 'search_results' : [user.to_json() for user in users] })
+    return { 'search_results' : [user.to_json() for user in users] }
