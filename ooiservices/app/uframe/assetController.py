d8fbfc8ecebf833a480fb87870deff1c9245ebdb
M@Campbell <mcampbell@asascience.com>
2015-04-29 13:39:36 -0400

    Adds new route to uframe blueprint: /uframe/events/<string:ref_des>

--- ooiservices/app/uframe/assetController.py
+++ ooiservices/app/uframe/assetController.py
@@ -708,6 +708,53 @@ def get_event(id):
         pass
     return jsonify(**data)
 
+#Read (object)
+@api.route('/events/<string:ref_des>', methods=['GET'])
+def get_events_by_ref_des(ref_des):
+    '''
+    Return all the events and their cooresponding asset ID when
+    provided a reference designator (Logical Instrument).
+    '''
+    #Setup the cache, just in case
+    cached = cache.get('events_by_ref_des')
+    if cached:
+        return cached
+
+    #Create the container for the processed response
+    result = {}
+
+    #variables used in loop
+    platform = ""
+    mooring = ""
+    instrument = ""
+
+    #Get all the events to begin searching though...
+    uframe_obj = uFrameEventCollection()
+    data = uframe_obj.to_json()
+    for row in data:
+        try:
+            if  row['referenceDesignator']['subsite'] is not None:
+                platform = row['referenceDesignator']['subsite']
+            if row['referenceDesignator']['node'] is not None:
+                mooring = row['referenceDesignator']['node']
+            if row['referenceDesignator']['sensor'] is not None:  
+                instrument = row['referenceDesignator']['sensor']
+            concat_ref_des =  '-'.join([platform, mooring, instrument])
+            if concat_ref_des == ref_des:
+                result['ref_des'] = concat_ref_des
+                result['start_date'] = row['startDate']
+                result['class'] = row['@class']
+                result['event_description'] = row['eventDescription']
+                result['event_type'] = row['eventType']
+                result['notes'] = row['notes']
+                result['url'] =  url_for('uframe.get_event', id=row['eventId'])
+                result['uframe_url'] = current_app.config['UFRAME_ASSETS_URL'] + '/%s/%s' % (uframe_obj.__endpoint__, row['eventId'])
+        except:
+            print "nope! %s, %s" % (concat_ref_des, ref_des)
+    
+    return jsonify(**result)
+
+
 #Create
 @auth.login_required
 @api.route('/events', methods=['POST'])
