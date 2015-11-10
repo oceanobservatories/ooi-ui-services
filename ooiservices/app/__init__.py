f49d33d2be189b0dafb6d5a425ef2cd98f8db74c
James Case <case@oceanz.org>
2015-11-10 15:35:20 -0500

    Adds query to support new triggered A&A display. Adds REDIS_URL config var to init.

--- ooiservices/app/__init__.py
+++ ooiservices/app/__init__.py
@@ -21,7 +21,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 
-cache = Cache(config={'CACHE_TYPE': 'redis'})
+cache = Cache()
 db = SQLAlchemy()
 make_searchable()
 csrf = CsrfProtect()
@@ -38,6 +38,10 @@ def create_app(config_name):
         env.from_yaml(os.path.join(basedir, 'config.yml'))
     celery.conf.update(BROKER_URL=app.config['REDIS_URL'],
                 CELERY_RESULT_BACKEND=app.config['REDIS_URL'])
+    if app.config['REDIS_CACHE']:
+        cache.config = {'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': app.config['REDIS_URL']}
+    else:
+        cache.config = {'CACHE_TYPE': 'simple'}
 
     #Adding logging capabilities.
     if app.config['LOGGING'] == True:
