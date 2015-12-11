fb3f6aaa04dee71cc1a5b5e8c143873150802f29
M@Campbell <mcampbell@asascience.com>
2015-12-10 19:16:57 -0500

    setup for reverse callback to the UI to handle the respons for token generation from CILogon.  Removed twitter and facebook classes

--- ooiservices/app/main/oauth.py
+++ ooiservices/app/main/oauth.py
@@ -1,5 +1,5 @@
-from rauth import OAuth1Service, OAuth2Service
-from flask import current_app, url_for, request, redirect, session
+from rauth import OAuth2Service
+from flask import current_app, request, redirect
 import json
 
 
@@ -18,8 +18,7 @@ class OAuthSignIn(object):
         pass
 
     def get_callback_url(self):
-        return url_for('.oauth_callback', provider=self.provider_name,
-                       _external=True)
+        return current_app.config['UI_URL'] + '/callback/cilogon'
 
     @classmethod
     def get_provider(self, provider_name):
@@ -29,78 +28,6 @@ class OAuthSignIn(object):
                 provider = provider_class()
                 self.providers[provider.provider_name] = provider
         return self.providers[provider_name]
-
-
-class FacebookSignIn(OAuthSignIn):
-    def __init__(self):
-        super(FacebookSignIn, self).__init__('facebook')
-        self.service = OAuth2Service(
-            name='facebook',
-            client_id=self.consumer_id,
-            client_secret=self.consumer_secret,
-            authorize_url='https://graph.facebook.com/oauth/authorize',
-            access_token_url='https://graph.facebook.com/oauth/access_token',
-            base_url='https://graph.facebook.com/'
-        )
-
-    def authorize(self):
-        return redirect(self.service.get_authorize_url(
-            scope='email',
-            response_type='code',
-            redirect_uri=self.get_callback_url())
-        )
-
-    def callback(self):
-        if 'code' not in request.args:
-            return None, None, None
-        oauth_session = self.service.get_auth_session(
-            data={'code': request.args['code'],
-                  'grant_type': 'authorization_code',
-                  'redirect_uri': self.get_callback_url()}
-        )
-        me = oauth_session.get('me?fields=id,email').json()
-        return (
-            'facebook$' + me['id'],
-            me.get('email').split('@')[0],  # Facebook does not provide
-                                            # username, so the email's user
-                                            # is used instead
-            me.get('email')
-        )
-
-
-class TwitterSignIn(OAuthSignIn):
-    def __init__(self):
-        super(TwitterSignIn, self).__init__('twitter')
-        self.service = OAuth1Service(
-            name='twitter',
-            consumer_key=self.consumer_id,
-            consumer_secret=self.consumer_secret,
-            request_token_url='https://api.twitter.com/oauth/request_token',
-            authorize_url='https://api.twitter.com/oauth/authorize',
-            access_token_url='https://api.twitter.com/oauth/access_token',
-            base_url='https://api.twitter.com/1.1/'
-        )
-
-    def authorize(self):
-        request_token = self.service.get_request_token(
-            params={'oauth_callback': self.get_callback_url()}
-        )
-        session['request_token'] = request_token
-        return redirect(self.service.get_authorize_url(request_token[0]))
-
-    def callback(self):
-        request_token = session.pop('request_token')
-        if 'oauth_verifier' not in request.args:
-            return None, None, None
-        oauth_session = self.service.get_auth_session(
-            request_token[0],
-            request_token[1],
-            data={'oauth_verifier': request.args['oauth_verifier']}
-        )
-        me = oauth_session.get('account/verify_credentials.json').json()
-        social_id = 'twitter$' + str(me.get('id'))
-        username = me.get('screen_name')
-        return social_id, username, None   # Twitter does not provide email
 
 
 class CILogonSignIn(OAuthSignIn):
@@ -133,8 +60,6 @@ class CILogonSignIn(OAuthSignIn):
 
     def callback(self):
         print 'Begin callback'
-        print self.get_callback_url()
-        print request.args
         if 'code' not in request.args:
             return None, None, None
         print request.args['code'].rsplit('authzGrant', 1)[-1]
@@ -145,28 +70,16 @@ class CILogonSignIn(OAuthSignIn):
         print data
 
         oauth_session = self.service.get_auth_session(
-            data=data
-            ,
+            data=data,
             decoder=self.custom_decoder
         )
 
         # return oauth_session.client_id
-        me = oauth_session.client_id
-        print 'me: ' + me
-        print oauth_session.__attrs__
-
-        me_profile = oauth_session.get('https://test.cilogon.org/oauth2/userinfo', params={'format': 'json'}).json()
-        print me_profile
-        print me_profile['email']
-
+        ci_logon_url = 'https://test.cilogon.org/oauth2/userinfo'
+        me_profile = oauth_session.get(ci_logon_url,
+                                       params={'format': 'json'}).json()
         print 'End callback'
 
-        return me_profile['email'], me_profile['given_name'], me_profile['family_name']
-        # me = oauth_session.get('me?fields=id,email').json()
-        # return (
-        #     'facebook$' + me['id'],
-        #     me.get('email').split('@')[0],  # Facebook does not provide
-        #                                     # username, so the email's user
-        #                                     # is used instead
-        #     me.get('email')
-        # )
+        return me_profile['email'], \
+            me_profile['given_name'], \
+            me_profile['family_name']
