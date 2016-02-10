1b4d3c6208ba4e718d04b4d4c9c0043c11148287
M@Campbell <mcampbell@asascience.com>
2016-02-10 08:17:14 -0500

    consolidated the route for easier access from the UI

--- ooiservices/app/main/sys.py
+++ ooiservices/app/main/sys.py
@@ -31,71 +31,72 @@ def list_routes():
     return jsonify({'routes': routes})
 
 
-@api.route('/cache_keys', methods=['GET'])
-def get_cache_list():
-    # setup the container for the list of current cache items
-    flask_cache = []
+@api.route('/cache_keys', methods=['GET', 'POST'])
+def cache_list():
+    '''
+    @method GET: returns this list of flask cache keys and their
+                 time to live (TTL)
+    @method POST: Submitting a post request with a list of keys
+                  will delete each key in the list
+                  Example: {delete: [<cache_item_1>, <cache_item_2>, ...]}
+    '''
+    if request.method == 'GET':
+        # setup the container for the list of current cache items
+        flask_cache = []
 
-    # issue the command to get the list from redis
-    pipe_output = subprocess.Popen(['redis-cli', 'scan', '0'],
-                                   stdout=subprocess.PIPE)
-    output, err = pipe_output.communicate()
-
-    # the output is a string, so lets load it into the array based
-    # in it's delimiter
-    flask_cache = output.split('\n')
-
-    # we don't want to provide system level redis keys, so iterate
-    # over the list and only provide those that are prefixed
-    # by 'flask_cache'
-    temp_list = []
-    for cache_key in flask_cache:
-        if 'flask_cache' in cache_key:
-            temp_list.append({'key': cache_key})
-
-    # assign the list to the main bin
-    flask_cache = temp_list
-
-    # lets get the TTL of each of the keys so we can see how long ago
-    # they were created.
-    for cache_key in flask_cache:
-        pipe_output = subprocess.Popen(['redis-cli', 'TTL', cache_key['key']],
+        # issue the command to get the list from redis
+        pipe_output = subprocess.Popen(['redis-cli', 'scan', '0'],
                                        stdout=subprocess.PIPE)
         output, err = pipe_output.communicate()
-        cache_key['TTL'] = output.split('\n')[0]
 
-    # clear out this for garbage collection of flask_cache ref
-    temp_list = None
+        # the output is a string, so lets load it into the array based
+        # in it's delimiter
+        flask_cache = output.split('\n')
 
-    return jsonify({'cache_list': flask_cache}), 200
+        # we don't want to provide system level redis keys, so iterate
+        # over the list and only provide those that are prefixed
+        # by 'flask_cache'
+        temp_list = []
+        for cache_key in flask_cache:
+            if 'flask_cache' in cache_key:
+                temp_list.append({'key': cache_key})
 
+        # assign the list to the main bin
+        flask_cache = temp_list
 
-@api.route('/delete_cache', methods=['POST'])
-def delete_cache():
-    '''
-    Example Post:
-    {delete: [<cache_item_1>, <cache_item_2>, ...]}
-
-    '''
-
-    redis_response = 1
-
-    try:
-        # grab the json and load the payload
-        redis_list = json.loads(request.data)
-
-        # for each item in the list, delete the cache
-        for cache_key in redis_list['delete']:
-            pipe_output = subprocess.Popen(['redis-cli', 'del', cache_key],
+        # lets get the TTL of each of the keys so we can see how long ago
+        # they were created.
+        for cache_key in flask_cache:
+            pipe_output = subprocess.Popen(['redis-cli', 'TTL',
+                                            cache_key['key']],
                                            stdout=subprocess.PIPE)
             output, err = pipe_output.communicate()
+            cache_key['TTL'] = output.split('\n')[0]
 
-            # figure out if the response is a great success
-            # by multiplying the redis_response by the output
-            # which will be a 1 for success and a 0 otherwise
-            redis_response = redis_response*output
+        # clear out this for garbage collection of flask_cache ref
+        temp_list = None
 
-        return jsonify({'response': int(redis_response)}), 200
+        return jsonify({'cache_list': flask_cache}), 200
 
-    except Exception as e:
-        return jsonify({'error': 'Exception in cache deletion. %s' % e}), 500
+    elif request.method == 'POST':
+        redis_response = 1
+
+        try:
+            # grab the json and load the payload
+            redis_list = json.loads(request.data)
+
+            # for each item in the list, delete the cache
+            for cache_key in redis_list['delete']:
+                pipe_output = subprocess.Popen(['redis-cli', 'del', cache_key],
+                                               stdout=subprocess.PIPE)
+                output, err = pipe_output.communicate()
+
+                # figure out if the response is a great success
+                # by multiplying the redis_response by the output
+                # which will be a 1 for success and a 0 otherwise
+                redis_response = redis_response*output
+
+            return jsonify({'response': int(redis_response)}), 200
+
+        except Exception as e:
+            return jsonify({'error': 'Exception in cache delete. %s' % e}), 500
