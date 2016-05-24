43d0539bfe757a85c4d2f51fc45a3887a9b54233
James Case <case@oceanz.org>
2016-05-24 15:35:53 -0400

    Improves streams and assets responses.

--- ooiservices/app/celeryconfig.py
+++ ooiservices/app/celeryconfig.py
@@ -1,8 +1,18 @@
 from celery.schedules import crontab
 
 CELERYBEAT_SCHEDULE = {
+    'get-assets-rd': {
+        'task': 'tasks.compile_asset_rds',
+        'schedule': crontab(minute=0, hour='*/1'),
+        'args': (),
+        },
     'get-assets': {
         'task': 'tasks.compile_assets',
+        'schedule': crontab(minute=0, hour='*/2'),
+        'args': (),
+        },
+    'get-bad-assets': {
+        'task': 'tasks.compile_bad_assets',
         'schedule': crontab(minute=0, hour='*/2'),
         'args': (),
         },
